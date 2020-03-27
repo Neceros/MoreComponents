4dc3057eaa6e818ede29947dea62beb0f15c0991
Neceros <neceros@gmail.com>
2020-03-26 20:33:28 -0700

    Initial commit

---
+++ Source/ModSettings.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace MoreComps
+{
+  public class MoreCompsModSettings : ModSettings
+  {
+    public static float MultiplyMineableComps => multiplyMC;
+    public static int OriginalCompsAmount;
+
+    // Internal reference only. DO NOT call outside of this class.
+    public static float multiplyMC = 2.5f;
+    // End warning
+
+    public override void ExposeData()
+    {
+      base.ExposeData();
+      Scribe_Values.Look(ref multiplyMC, "MCAmountToMultiplyComps");
+    }
+  }
+
+  public class MoreCompsMod : Mod
+  {
+    MoreCompsModSettings settings;
+    public MoreCompsMod(ModContentPack con) : base(con)
+    {
+      this.settings = GetSettings<MoreCompsModSettings>();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+      Listing_Standard listing = new Listing_Standard();
+      listing.Begin(inRect);
+      listing.Label("MCRestartWarning".Translate());
+      listing.Gap(24);
+      listing.Label("MCMultiplyAmountLabel".Translate() + ": [" + MoreCompsModSettings.OriginalCompsAmount.ToString() + " x " + (MoreCompsModSettings.multiplyMC * 100).ToString() + "%] = " + (MoreCompsModSettings.OriginalCompsAmount * MoreCompsModSettings.multiplyMC).ToString() + " " + "MCMultiplyAmountEndLabel".Translate());
+      MoreCompsModSettings.multiplyMC = RoundToNearestHalf(listing.Slider(MoreCompsModSettings.multiplyMC, 0f, 20f));
+      if(MoreCompsModSettings.OriginalCompsAmount * MoreCompsModSettings.multiplyMC >= 75)
+        listing.Label("MCStackWarning".Translate());
+      
+      listing.End();
+      base.DoSettingsWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+      return "MCTitle".Translate();
+    }
+
+    private float RoundToNearestHalf(float val)
+    { 
+      return (float)Math.Round(val * 2, MidpointRounding.AwayFromZero) / 2;
+    }
+  }
+}
---
+++ Source/WorldComponent.cs
@@ -0,0 +1,26 @@
+﻿using RimWorld.Planet;
+using System;
+using Verse;
+
+namespace MoreComps
+{
+  internal class MoreCompsWorldComp : WorldComponent
+  {
+    public MoreCompsWorldComp(World world)
+      : base(world)
+    {}
+
+    public override void FinalizeInit()
+    {
+      base.FinalizeInit();
+
+      MoreCompsModSettings.OriginalCompsAmount = DefDatabase<ThingDef>.GetNamed("MineableComponentsIndustrial").building.mineableYield;
+      ApplyAllChanges();
+    }
+
+    public void ApplyAllChanges()
+    {
+      DefDatabase<ThingDef>.GetNamed("MineableComponentsIndustrial").building.mineableYield = (int)Math.Floor(MoreCompsModSettings.OriginalCompsAmount * MoreCompsModSettings.multiplyMC);
+    }
+  }
+}
